99f5efcb37632cbd17cf364f67a156c14c24fb43
HafidzFairuz24 <hafidzfairuz4@gmail.com>
2026-05-14 10:09:55 +0700

    Fix auth middleware import and add bug reporting feature

--- BACKEND/src/routes/bugs.js
+++ BACKEND/src/routes/bugs.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bugController = require('../controllers/bugController');
-const { verifyToken } = require('../middleware/auth');
+const authMiddleware = require('../middleware/auth');
 
-router.post('/', verifyToken, bugController.createBugReport);
-router.get('/', verifyToken, bugController.getAllBugReports);
+router.post('/', authMiddleware, bugController.createBugReport);
+router.get('/', authMiddleware, bugController.getAllBugReports);
 
 module.exports = router;
